a633d48a4649f6010fafa2f11b9c82cb31c0b69d
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2023-05-18 10:50:27 +0900

    Create LABweek12_2.cpp

---
+++ Data_Structure_LAB/week12/LABweek12_2.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct QueueNode {	
+	int data;
+	struct QueueNode* link;
+} QueueNode;
+
+typedef struct {
+	QueueNode* rear;
+} LinkedQueueType;
+
+void init(LinkedQueueType* q) {
+	q->rear = NULL;
+}
+
+int isQueueEmpty(LinkedQueueType* q) {
+	return q->rear == NULL;
+}
+
+void enqueue(LinkedQueueType* q, int data) {
+	QueueNode* p = (QueueNode*)malloc(sizeof(QueueNode));
+	p->data = data;
+	p->link = NULL;
+
+	if (isQueueEmpty(q)) {	
+		q->rear = p;
+		p->link = p; 
+	}
+	else {
+		p->link = q->rear->link;  
+		q->rear->link = p;  
+		q->rear = p; 
+	}
+}
+
+int dequeue(LinkedQueueType* q) {
+	if (isQueueEmpty(q)) {
+		printf("Queue is empty.\n");
+		return -1;
+	}
+
+	QueueNode* p = q->rear->link;
+	int data = p->data;
+
+	if (p == q->rear) {
+		q->rear = NULL;  
+	}
+	else {
+		q->rear->link = p->link;
+	}
+
+	free(p);
+	return data;
+}
+
+void printQueue(LinkedQueueType* q) {
+	if (isQueueEmpty(q)) {
+		printf("Queue is empty.\n");
+		return;
+	}
+
+
+	QueueNode* p = q->rear->link;
+	printf("큐: ");
+
+	do {
+		printf("%d ", p->data);
+		p = p->link;
+	} while (p != q->rear->link);
+
+
+	printf("rear: %d\n", q->rear->data);
+}
+
+int main() {
+	LinkedQueueType q;
+	init(&q);
+
+	enqueue(&q, 1); printQueue(&q);
+	enqueue(&q, 2); printQueue(&q);
+	enqueue(&q, 3); printQueue(&q);
+	dequeue(&q);    printQueue(&q);
+	enqueue(&q, 4); printQueue(&q);
+	dequeue(&q);    printQueue(&q);
+	dequeue(&q);    printQueue(&q);
+	enqueue(&q, 5); printQueue(&q);
+
+	return 0;
+}
