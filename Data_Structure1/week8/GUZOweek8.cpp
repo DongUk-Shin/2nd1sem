cb0ec6f38de197f870e7b1ea73dc92d1dfd60a48
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2023-05-23 20:36:57 +0900

    Update GUZOweek8.cpp

--- Data_Structure1/week8/GUZOweek8.cpp
+++ Data_Structure1/week8/GUZOweek8.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
 #define MAX_SIZE 100
 typedef struct{
 	char stk[MAX_SIZE];
@@ -17,7 +17,6 @@ int is_empty(stack_type s) {
 }
 
 int is_full(stack_type s) {
-
 	return (s.top == MAX_SIZE - 1);
 }
 
@@ -55,27 +54,25 @@ double eval(char ch, double opnd1, double opnd2) {
 		return opnd1 * opnd2;
 	case '/':
 		return opnd1 / opnd2;
+    case '^':  // 거듭제곱 연산 추가
+        return pow(opnd1, opnd2);
 	default:
 		break;
 	}
 }
 
-double eval_postfix(char exp[])
-{
+double eval_postfix(char exp[]) {
     stack_type s;
     init(&s);
     int i = 0;
     char ch;
     double oper1, oper2;
 
-    while ((ch = exp[i++]) != '\0')
-    {
-        if (isdigit(ch))
-        {
+    while ((ch = exp[i++]) != '\0') {
+        if (isdigit(ch)) {
             push(&s, (double)(ch - '0'));
-        }
-        else
-        {
+        } 
+        else {
             oper2 = pop(&s);
             oper1 = pop(&s);
             push(&s, eval(ch, oper1, oper2));
@@ -85,59 +82,124 @@ double eval_postfix(char exp[])
 }
 
 int pie(char oper) {
-    if (oper == '(') {
+    if (oper == '(' || oper == '[' || oper == '{') {
+        return 4;
+    } 
+    else if (oper == '^') {
         return 3;
-    } else if (oper == '*' || oper == '/') {
+    } 
+    else if (oper == '*' || oper == '/') {
         return 2;
-    } else if (oper == '+' || oper == '-') {
+    } 
+    else if (oper == '+' || oper == '-') {
         return 1;
-    } else {
+    } 
+    else {
         return 0;
     }
 }
 
 int pis(char oper) {
-    if (oper == '(') {
+    if (oper == '(' || oper == '[' || oper == '{') {
         return 0;
-    } else if (oper == '*' || oper == '/') {
+    } 
+    else if (oper == '^') {
+        return 3;
+    } 
+    else if (oper == '*' || oper == '/') {
         return 2;
-    } else if (oper == '+' || oper == '-') {
+    } 
+    else if (oper == '+' || oper == '-') {
         return 1;
-    } else {
+    } 
+    else {
         return -1;
     }
 }
 
-char* postfix(char exp[], char pexp[]) {
+char get_token(char exp[]) { //문자열의 자리수를 식별 후 반환
+    static int pos = 0;
+    char token = exp[pos];
+
+    if (token == '\0') {
+        pos = 0;
+        return token;
+    }
+    else if (isdigit(token)) {
+        pos++;
+        return token;
+    }
+    else if (token == '+' || token == '-' || token == '*' || 
+             token == '/' || token == '(' || token == ')' || 
+             token == '[' || token == ']' || token == '{' || 
+             token == '}' || token == '^') {
+        pos++;
+        return token;
+    }
+    else if (isspace(token)) { //공백이면 다음 토큰 호출 (재귀)
+        pos++;
+        return get_token(exp); 
+    }
+    else {
+        printf("수식 오류\n");
+        return 0;
+    }
+}
+
+char* postfix(char exp[], char pexp[]) { //중위 -> 후위 함수
     int i = 0, j = 0;
-    char ch;
     stack_type s;
     init(&s);
+    char ch = get_token(exp);
 
-    while ((ch = exp[i++]) != '\0') {
-        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+    while (ch != '\0') {
+        if (ch == '(' || ch == '[' || ch == '{') {
+            push(&s, ch);
+        }
+        else if (ch == ')' || ch == ']' || ch == '}') {
+            while (!is_empty(s) && peek(s) != '(' && peek(s) != '[' && peek(s) != '{') {
+                pexp[j++] = pop(&s);
+            }
+            if (!is_empty(s) && ((ch == ')' && peek(s) == '(') || 
+                                 (ch == ']' && peek(s) == '[') || 
+                                 (ch == '}' && peek(s) == '{'))) {
+                pop(&s); // 왼쪽 괄호 제거
+            }
+        }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^') {
             while (!is_empty(s) && pis(peek(s)) >= pie(ch)) {
                 pexp[j++] = pop(&s);
             }
             push(&s, ch);
         }
-        else {
+        else { //ch가 숫자일 때 
             pexp[j++] = ch;
         }
+        ch = get_token(exp); //get_token 호출하여 exp의 다음 인덱스 반환
     }
     while (!is_empty(s)) {
         pexp[j++] = pop(&s);
     }
-    pexp[j++] = '\0';
+    pexp[j] = '\0';
     return pexp;
 }
 
-int main(void)
-{
-    char exp[MAX_SIZE] = "5*2+3-4/2";
+char* get_exp(char exp[]) { //수식 입력 함수
+    printf("수식을 입력하세요: ");
+    scanf("%[^\n]", exp);
+    return exp;
+}
+
+int main(void) {   
+    char exp[MAX_SIZE];
     char pexp[MAX_SIZE];
 
+    get_exp(exp);
+    
+    char a[MAX_SIZE];
+    int i = 0, j= 1;
     double result = eval_postfix(postfix(exp, pexp));
-    printf("postfix: %s\n", pexp);
-    printf("eval_postfix: %.1f\n", result);
+
+    printf("후위 변환 : %s\n", pexp);
+    printf("계산 결과 : %.1f\n", result);
 }
