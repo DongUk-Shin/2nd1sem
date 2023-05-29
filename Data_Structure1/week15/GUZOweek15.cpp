88d43891bb3b6ecfe35bf7be3d27247f3f8721d7
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2023-05-29 22:28:46 +0900

    Update GUZOweek15.cpp

--- Data_Structure1/week15/GUZOweek15.cpp
+++ Data_Structure1/week15/GUZOweek15.cpp
@@ -1,7 +1,6 @@
 //계산기 입력(중위) -> 후위 -> 트리 -> 트리연산
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #define MAX_SIZE 100
 typedef struct{
@@ -39,49 +38,49 @@ int peek(stack_type s) {
 	return (s).stk[(s).top];
 }
 
-typedef struct TreeNode {
-    int data;
-    struct TreeNode *left, *right;
-} TreeNode;
-
-int pie(char oper) { //우선순위
-    if (oper == '(') {
+int pie(char oper) {
+    if (oper == '(' || oper == '[' || oper == '{') {
         return 3;
-    } else if (oper == '*' || oper == '/') {
+    } 
+    else if (oper == '*' || oper == '/') {
         return 2;
-    } else if (oper == '+' || oper == '-') {
+    } 
+    else if (oper == '+' || oper == '-') {
         return 1;
-    } else {
+    } 
+    else {
         return 0;
     }
 }
 int pis(char oper) {
-    if (oper == '(') {
+    if (oper == '(' || oper == '[' || oper == '{') {
         return 0;
-    } else if (oper == '*' || oper == '/') {
+    } 
+    else if (oper == '*' || oper == '/') {
         return 2;
-    } else if (oper == '+' || oper == '-') {
+    } 
+    else if (oper == '+' || oper == '-') {
         return 1;
-    } else {
+    } 
+    else {
         return -1;
     }
 }
-
-char* postfix(char exp[], char pexp[]) { //중위 -> 후위 함수
+char* postfix(char exp[], char post[]) { //중위 -> 후위 함수
     int i = 0, j = 0;
     char ch;
     stack_type s;
     init(&s);
     
     while ((ch = exp[i++]) != '\0') {
-        if (ch == ' ') continue; //공백일 시 continue
+        if (ch == ' ') continue; //공백일 시 pass
 
-        if (ch == '(' || ch == '[' || ch == '{') {
+        if (ch == '(' || ch == '[' || ch == '{') 
             stack_push(&s, ch);
-        }
+        
         else if (ch == ')' || ch == ']' || ch == '}') {
             while (!is_empty(s) && peek(s) != '(' && peek(s) != '[' && peek(s) != '{') {
-                pexp[j++] = stack_pop(&s);
+                post[j++] = stack_pop(&s);
             }
             if (!is_empty(s) && ((ch == ')' && peek(s) == '(') || 
                                  (ch == ']' && peek(s) == '[') || 
@@ -89,21 +88,21 @@ char* postfix(char exp[], char pexp[]) { //중위 -> 후위 함수
                 stack_pop(&s); // 왼쪽 괄호 제거
             }
         }
-        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^') {
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' ) {
             while (!is_empty(s) && pis(peek(s)) >= pie(ch)) {
-                pexp[j++] = stack_pop(&s);
+                post[j++] = stack_pop(&s);
             }
             stack_push(&s, ch);
         }
         else { //ch가 숫자일 때 
-            pexp[j++] = ch;
+            post[j++] = ch;
         }
     }
     while (!is_empty(s)) {
-        pexp[j++] = stack_pop(&s);
+        post[j++] = stack_pop(&s);
     }
-    pexp[j] = '\0';
-    return pexp;
+    post[j] = '\0';
+    return post;
 }
 
 char* get_exp(char exp[]) { //수식 입력 함수
@@ -111,8 +110,12 @@ char* get_exp(char exp[]) { //수식 입력 함수
     scanf("%[^\n]", exp);
     return exp;
 }
-
 /*-------------------------*/
+
+typedef struct TreeNode { //트리 노드
+    int data;
+    struct TreeNode *left, *right;
+} TreeNode;
 
 #define SIZE 100
 int top = -1;
@@ -139,59 +142,51 @@ int eval_oper(int opnd1, int opnd2, char oper) { //연산자 계산
         return opnd1 * opnd2;
     case '/':
         return opnd1 / opnd2;
-    case '^':
-        return pow(opnd1, opnd2);
     }
 }
 
-int eval(TreeNode *root) { //노드 두개 연산
+int eval(TreeNode *root) { //노드 두개 연산 (전위)
     if (root == NULL) 
         return 0;
     if (root->left == NULL && root->right == NULL) 
-        return root->data;
-    else {
-        int opnd1 = eval(root->left);
-        int opnd2 = eval(root->right);
-        printf("노드 두개 연산: %d %c %d \n", opnd1, root->data, opnd2);
-        return eval_oper(opnd1, opnd2, root->data);
-    }
-    return 0;
+        return root->data; //단말 노드인가(정수인가)
+    int leftopnd = eval(root->left);
+    int rightopnd = eval(root->right);
+    printf("노드 연산: %d %c %d \n", leftopnd, root->data, rightopnd);
+    return eval_oper(leftopnd, rightopnd, root->data);
 }
 
-TreeNode* cons_exptree(char postfix[]) { //후위 -> 트리
-    stack_type s;
-    init(&s);
+TreeNode* cons_exptree(char post[]) { //후위 -> 트리
     int i = 0;
     char ch;
-
-    while ((ch = postfix[i++]) != '\0') {
+    printf("노드: ");
+    while ((ch = post[i++]) != '\0') {
         TreeNode *newNode = (TreeNode*)malloc(sizeof(TreeNode));
         newNode->data = ch;
         newNode->left = newNode->right = NULL;
-        
+        printf("[%c] ", ch);
         if (isdigit(ch)) {
             int num = ch - '0'; // 숫자 문자를 정수로 변환
             newNode->data = num;
-            printf("노드 :%d \n", num);
             tree_push(newNode);
+
         } else {
-            printf("노드 :%c \n", ch);
             newNode->right = tree_pop();
             newNode->left = tree_pop();
             tree_push(newNode);
         }
     }
+    printf("\n");
     return tree_pop();
 }
 
 int main(void) {
     char exp[MAX_SIZE];
     char post[MAX_SIZE];
-    get_exp(exp);
-    postfix(exp, post);
-    printf("후위: %s\n", post);
-    TreeNode *root = cons_exptree(post);
-
+    get_exp(exp); //수식 입력
+    postfix(exp, post); //중위식 -> 후위식
+    printf("후위: %s\n", post); 
+    TreeNode *root = cons_exptree(post); //후위식 -> 트리
     printf("계산 결과: %d \n", eval(root));
     return 0;
 }
